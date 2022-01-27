a200b9a8735115ca6d42aa9355f79b417c3ded7e
cbellmont <carlos.bellmont@gmail.com>
2022-01-27 15:29:05 +0100

    Main Classes added

---
+++ app/src/main/java/com/example/mockitotest/Persona.kt
@@ -0,0 +1,21 @@
+package com.example.mockitotest
+
+class Persona (
+    val nombre: String,
+) {
+
+    var hermano : Persona? = null
+
+    fun miNombreEnFrase() : String {
+        return "Me llamo $nombre"
+    }
+
+    fun tengoHermano() : Boolean {
+        return hermano != null
+    }
+
+    fun decirNombreHermano() : String {
+        return hermano?.nombre.orEmpty()
+    }
+
+}
